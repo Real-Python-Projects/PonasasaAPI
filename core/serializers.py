6c0108813b3c404e1a20e4414c72dc7c7abf85d6
Developer-felix <onjombafelix1@gmail.com>
2021-05-29 03:20:24 -0600

    Solving custom user error serialisation

--- core/serializers.py
+++ core/serializers.py
@@ -2,6 +2,9 @@ from rest_framework import serializers, status
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
 from .models import *
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class UserSerializer(serializers.ModelSerializer):
