288924cf9971aedbf0f0da021e980c42f7933103
developer-felix <onjombafelix1@gmail.com>
2021-06-08 02:13:59 -0600

    Admin Mpesa register models

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+
+# Register your models here.
+
 from django.contrib import admin
 from .models import *
 # Register your models here.
 
+
+
+
 admin.site.site_title = "Ponasasa"
 admin.site.site_header = "Ponasasa"
+
+
+
+class PaymentTransactionAdmin(admin.ModelAdmin):
+    list_display = ("phone_number", "amount", "isFinished",
+                    "isSuccessFull", "trans_id", 'date_created','date_modified')
+
+admin.site.register(PaymentTransaction, PaymentTransactionAdmin)
+admin.site.register(Wallet)
 
 admin.site.register(PharmacyOwnerProfile)
 admin.site.register(PharmacistProfile)
