16949d3ea6fc4d6aefcb01bc964cb100a9bd949c
developer-felix <onjombafelix1@gmail.com>
2021-06-09 08:32:53 -0600

    Title change

--- ponasasa/urls.py
+++ ponasasa/urls.py
@@ -9,7 +9,7 @@ from django.urls import path,include
 from rest_framework.documentation import include_docs_urls
 from rest_framework_swagger.views import get_swagger_view
 
-schema_view = get_swagger_view(title="Swagger Docs")
+schema_view = get_swagger_view(title="Ponasasa API documentation")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
